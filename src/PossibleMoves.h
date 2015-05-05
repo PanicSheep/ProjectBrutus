d47e0e13af8bf16d67503268c5735b1976fd17f6
PanicSheep <PanicSheep@users.noreply.github.com>
2015-05-05 22:35:28 +0200

    get_some_moves templated and uint64_t HasMoves

--- src/PossibleMoves.h
+++ src/PossibleMoves.h
@@ -1,6 +1,32 @@
 #pragma once
-#include <string>
+#include <stdint.h>
 
-inline std::string PossibleMoves_version() { return std::string("Version 3.0 ").append(__DATE__).append(" Advanced Kogge-Stone parallel prefix"); }
-bool HasMoves(const unsigned long long P, const unsigned long long O);
-unsigned long long PossibleMoves(const unsigned long long P, const unsigned long long O);
+uint64_t HasMoves(const uint64_t P, const uint64_t O);
+uint64_t PossibleMoves(const uint64_t P, const uint64_t O);
+
+template <const int dir>
+inline uint64_t get_some_moves(const uint64_t P, const uint64_t mask)
+{
+	// kogge-stone parallel prefix
+	// 12 x SHIFT, 10 x AND, 7 x OR
+	// = 29 OPs
+	uint64_t flip_l, flip_r;
+	uint64_t mask_l, mask_r;
+
+	flip_l  = mask & (P << dir);
+	flip_r  = mask & (P >> dir);
+
+	flip_l |= mask & (flip_l << dir);
+	flip_r |= mask & (flip_r >> dir);
+
+	mask_l  = mask & (mask << dir);
+	mask_r  = mask & (mask >> dir);
+
+	flip_l |= mask_l & (flip_l << (dir * 2));
+	flip_r |= mask_r & (flip_r >> (dir * 2));
+
+	flip_l |= mask_l & (flip_l << (dir * 2));
+	flip_r |= mask_r & (flip_r >> (dir * 2));
+
+	return (flip_l << dir) | (flip_r >> dir);
+}
