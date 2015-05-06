70fff30390ee7f6c1130886cf29893e0e24e9ab4
PanicSheep <PanicSheep@users.noreply.github.com>
2015-05-06 15:46:11 +0200

    minor updates

--- src/utility.h
+++ src/utility.h
@@ -19,6 +19,11 @@ inline int RoundInt(const float  f) { return static_cast<int>(std::roundf(f)); }
 //inline int RoundInt(double d) { int i = static_cast<int>(d); return i + (d - i >= .5 ) - (d - i <= -.5 ); }
 //inline int RoundInt(float  f) { int i = static_cast<int>(f); return i + (f - i >= .5f) - (f - i <= -.5f); }
 
+const uint64_t START_POSITION_P = 0x0000000810000000ULL;
+const uint64_t START_POSITION_O = 0x0000001008000000ULL;
+const uint64_t START_POSITION_ETH_P = 0x0000001800000000ULL;
+const uint64_t START_POSITION_ETH_O = 0x0000000018000000ULL;
+
 static const std::string field_name[65] = {
 	"A1", "B1", "C1", "D1", "E1", "F1", "G1", "H1",
 	"A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2",
@@ -29,7 +34,7 @@ static const std::string field_name[65] = {
 	"A7", "B7", "C7", "D7", "E7", "F7", "G7", "H7",
 	"A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8", "--"
 };
-static const unsigned char field_type[64] = {   
+static const uint8_t field_type[64] = {   
 	0, 1, 2, 3, 3, 2, 1, 0,
 	1, 4, 5, 6, 6, 5, 4, 1,
 	2, 5, 7, 8, 8, 7, 5, 2,
@@ -39,7 +44,7 @@ static const unsigned char field_type[64] = {
 	1, 4, 5, 6, 6, 5, 4, 1,
 	0, 1, 2, 3, 3, 2, 1, 0
 };
-static const unsigned char quadrant_id_2_bit[64] = {   
+static const uint8_t quadrant_id_2_bit[64] = {   
 	0, 0, 0, 0, 1, 1, 1, 1,
 	0, 0, 0, 0, 1, 1, 1, 1,
 	0, 0, 0, 0, 1, 1, 1, 1,
@@ -49,7 +54,7 @@ static const unsigned char quadrant_id_2_bit[64] = {
 	2, 2, 2, 2, 3, 3, 3, 3,
 	2, 2, 2, 2, 3, 3, 3, 3
 };
-static const unsigned long long quadrant[16] = {   
+static const uint64_t quadrant[16] = {   
 	0x0000000000000000ULL, //0000
 	0x000000000F0F0F0FULL, //0001
 	0x00000000F0F0F0F0ULL, //0010
@@ -67,7 +72,7 @@ static const unsigned long long quadrant[16] = {
 	0xFFFFFFFFF0F0F0F0ULL, //1110
 	0xFFFFFFFFFFFFFFFFULL  //1111
 };
-static const unsigned char quadrant_id_4_bit[64] = {   
+static const uint8_t quadrant_id_4_bit[64] = {   
 	1, 1, 1, 1, 2, 2, 2, 2,
 	1, 1, 1, 1, 2, 2, 2, 2,
 	1, 1, 1, 1, 2, 2, 2, 2,
@@ -77,7 +82,7 @@ static const unsigned char quadrant_id_4_bit[64] = {
 	4, 4, 4, 4, 8, 8, 8, 8,
 	4, 4, 4, 4, 8, 8, 8, 8
 };
-static const unsigned char max_difference_1[64] = {   
+static const uint8_t max_difference_1[64] = {   
 	37, 33, 31, 29, 29, 31, 33, 37, 
 	33, 31, 29, 27, 27, 29, 31, 33, 
 	31, 29, 31, 29, 29, 31, 29, 31, 
@@ -90,7 +95,7 @@ static const unsigned char max_difference_1[64] = {
 
 CACHE_LINE_ALIGNMENT
 
-static const unsigned long long neighbour[64] = { // Neighbours to the input-field
+static const uint64_t neighbour[64] = { // Neighbours to the input-field
 	0x0000000000000302ULL, 0x0000000000000705ULL, 0x0000000000000e0aULL, 0x0000000000001c14ULL,	0x0000000000003828ULL, 0x0000000000007050ULL, 0x000000000000e0a0ULL, 0x000000000000c040ULL,
 	0x0000000000030203ULL, 0x0000000000070507ULL, 0x00000000000e0a0eULL, 0x00000000001c141cULL,	0x0000000000382838ULL, 0x0000000000705070ULL, 0x0000000000e0a0e0ULL, 0x0000000000c040c0ULL,
 	0x0000000003020300ULL, 0x0000000007050700ULL, 0x000000000e0a0e00ULL, 0x000000001c141c00ULL,	0x0000000038283800ULL, 0x0000000070507000ULL, 0x00000000e0a0e000ULL, 0x00000000c040c000ULL,
@@ -100,7 +105,7 @@ static const unsigned long long neighbour[64] = { // Neighbours to the input-fie
 	0x0302030000000000ULL, 0x0705070000000000ULL, 0x0e0a0e0000000000ULL, 0x1c141c0000000000ULL,	0x3828380000000000ULL, 0x7050700000000000ULL, 0xe0a0e00000000000ULL, 0xc040c00000000000ULL,
 	0x0203000000000000ULL, 0x0507000000000000ULL, 0x0a0e000000000000ULL, 0x141c000000000000ULL,	0x2838000000000000ULL, 0x5070000000000000ULL, 0xa0e0000000000000ULL, 0x40c0000000000000ULL
 };
-static const unsigned long long affectable[64] = { // Fields that could be affected by playing the input-field
+static const uint64_t affectable[64] = { // Fields that could be affected by playing the input-field
 	0x004121110905037EULL, 0x00024222120A067CULL, 0x0004044424140E7AULL, 0x00080808482A1C76ULL,	0x001010101254386EULL, 0x002020222428705EULL, 0x004042444850603EULL, 0x0082848890A0C07EULL,
 	0x0021110905037E00ULL, 0x004222120A067C00ULL, 0x00044424140E7A00ULL, 0x000808482A1C7600ULL,	0x0010101254386E00ULL, 0x0020222428705E00ULL, 0x0042444850603E00ULL, 0x00848890A0C07E00ULL,
 	0x00110905037E0300ULL, 0x0022120A067C0600ULL, 0x004424140E7A0E00ULL, 0x0008482A1C761C00ULL,	0x00101254386E3800ULL, 0x00222428705E7000ULL, 0x00444850603E6000ULL, 0x008890A0C07EC000ULL,
@@ -111,31 +116,32 @@ static const unsigned long long affectable[64] = { // Fields that could be affec
 	0x7E03050911214100ULL, 0x7C060A1222420200ULL, 0x7A0E142444040400ULL, 0x761C2A4808080800ULL,	0x6E38541210101000ULL, 0x5E70282422202000ULL, 0x3E60504844424000ULL, 0x7EC0A09088848200ULL
 };
 
-void REVERSE_BITORDER(unsigned long long & v);
-inline const std::string FIELD_NAME(const char Index) { return field_name[Index]; }
-unsigned char FIELD_INDEX(const std::string & s);
-inline unsigned char QUADRANT_ID(const char Index) { return quadrant_id_4_bit[Index]; }
-inline unsigned char QUADRANT_ID_2_BIT(const char Index) { return quadrant_id_2_bit[Index]; }
-inline unsigned long long QUADRANT(const char Index) { return quadrant[Index]; }
+void REVERSE_BITORDER(uint64_t & v);
+inline const std::string FIELD_NAME(const int Index) { return field_name[Index]; }
+uint8_t FIELD_INDEX(const std::string & s);
+inline uint8_t QUADRANT_ID(const int Index) { return quadrant_id_4_bit[Index]; }
+inline uint8_t QUADRANT_ID_2_BIT(const int Index) { return quadrant_id_2_bit[Index]; }
+inline uint64_t QUADRANT(const int Index) { return quadrant[Index]; }
 inline int MAX_DIFFERENCE_GAIN(const int Index) { return max_difference_1[Index]; }
-inline unsigned long long NEIGHBOUR(const int Index) { return neighbour[Index]; }
-unsigned long long NEIGHBOUR(const unsigned long long BitBoard);
-inline unsigned long long AFFECTABLE(const int Index){ return affectable[Index]; }
-unsigned long long AFFECTABLE(unsigned long long BitBoard);
-unsigned long long diagonal_flip(unsigned long long b);
-unsigned long long codiagonal_flip(unsigned long long b);
-unsigned long long vertical_flip(unsigned long long b);
-unsigned long long horizontal_flip(unsigned long long b);
+inline uint64_t NEIGHBOUR(const int Index) { return neighbour[Index]; }
+uint64_t NEIGHBOUR(const uint64_t BitBoard);
+inline uint64_t AFFECTABLE(const int Index){ return affectable[Index]; }
+uint64_t AFFECTABLE(uint64_t BitBoard);
+uint64_t diagonal_flip(uint64_t b);
+uint64_t codiagonal_flip(uint64_t b);
+uint64_t vertical_flip(uint64_t b);
+uint64_t horizontal_flip(uint64_t b);
 std::string SCIENTIFIC_NOTATION(double Number);
 std::string ScientificNotaion(long double value, long double error, int error_digits);
 std::string time_format(const std::chrono::milliseconds duration);
+inline std::string time_format(const std::chrono::duration<std::chrono::system_clock::rep, std::chrono::system_clock::period> duration) { return time_format(std::chrono::duration_cast<std::chrono::milliseconds>(duration)); }
 std::string short_time_format(std::chrono::duration<long long, std::pico> duration);
-std::string board(const unsigned long long P, const unsigned long long O);
-std::string board2D(const unsigned long long P, const unsigned long long O);
-void print_board(const unsigned long long, const unsigned long long);
-void print_board(const unsigned long long P, const unsigned long long O, const unsigned long long PossibleMoves);
+std::string board1D(const uint64_t P, const uint64_t O);
+std::string board2D(const uint64_t P, const uint64_t O);
+void print_board(const uint64_t, const uint64_t);
+void print_board(const uint64_t P, const uint64_t O, const uint64_t PossibleMoves);
 void print_progressbar(const int width, const float fraction);
 std::string progressbar_percentage(const int width, const float fraction);
-std::string ThousandsSeparator(unsigned long long n);
+std::string ThousandsSeparator(uint64_t n);
 std::string DateTimeNow();
-long long pow(long long base, unsigned long long exp);
+long long pow(long long base, uint64_t exp);
