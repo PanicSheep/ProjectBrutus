3228d5e5b22a27d83cca7dd9e8a634fc420d72c0
PanicSheep <PanicSheep@users.noreply.github.com>
2015-05-06 16:12:55 +0200

    Update position.cpp

--- src/position.cpp
+++ src/position.cpp
@@ -1,105 +1,9 @@
 #include "position.h"
 
-void PossibleMoves(const unsigned long long P, const unsigned long long O, unsigned long long & PossibleMovesP, unsigned long long & PossibleMovesO)
+uint64_t StableStones_full_edges_second_order(const uint64_t P, const uint64_t O)
 {
-	__m128i mask1, mask2, mask3, mask4, mask5, mask6, mask7, mask8, maskO1, maskO2, maskO3;
-    __m128i flip1, flip2, flip3, flip4, flip5, flip6, flip7, flip8;
-	__m128i A, B;
-
-	A = _mm_set_epi64x(P, O);
-	B = _mm_set_epi64x(O, P);
-
-	maskO1 = mask1 = mask2 = _mm_and_si128(B, _mm_set1_epi64x(0x7E7E7E7E7E7E7E7EULL));
-	maskO2 = mask3 = mask4 = _mm_and_si128(B, _mm_set1_epi64x(0x00FFFFFFFFFFFF00ULL));
-	maskO3 = mask5 = mask6 = mask7 = mask8 = _mm_and_si128(B, _mm_set1_epi64x(0x007E7E7E7E7E7E00ULL));
-
-	flip1 = _mm_or_si128(A, _mm_and_si128(maskO1, _mm_slli_epi64(A, 1))); // SHIFT, AND, OR
-	flip2 = _mm_or_si128(A, _mm_and_si128(maskO1, _mm_srli_epi64(A, 1))); // SHIFT, AND, OR
-	flip3 = _mm_or_si128(A, _mm_and_si128(maskO2, _mm_slli_epi64(A, 8))); // SHIFT, AND, OR
-	flip4 = _mm_or_si128(A, _mm_and_si128(maskO2, _mm_srli_epi64(A, 8))); // SHIFT, AND, OR
-	flip5 = _mm_or_si128(A, _mm_and_si128(maskO3, _mm_slli_epi64(A, 7))); // SHIFT, AND, OR
-	flip6 = _mm_or_si128(A, _mm_and_si128(maskO3, _mm_srli_epi64(A, 7))); // SHIFT, AND, OR
-	flip7 = _mm_or_si128(A, _mm_and_si128(maskO3, _mm_slli_epi64(A, 9))); // SHIFT, AND, OR
-	flip8 = _mm_or_si128(A, _mm_and_si128(maskO3, _mm_srli_epi64(A, 9))); // SHIFT, AND, OR
-
-    mask1 = _mm_and_si128(mask1, _mm_slli_epi64(maskO1, 1)); // SHIFT, AND
-    mask2 = _mm_and_si128(mask2, _mm_srli_epi64(maskO1, 1)); // SHIFT, AND
-    mask3 = _mm_and_si128(mask3, _mm_slli_epi64(maskO2, 8)); // SHIFT, AND
-    mask4 = _mm_and_si128(mask4, _mm_srli_epi64(maskO2, 8)); // SHIFT, AND
-    mask5 = _mm_and_si128(mask5, _mm_slli_epi64(maskO3, 7)); // SHIFT, AND
-    mask6 = _mm_and_si128(mask6, _mm_srli_epi64(maskO3, 7)); // SHIFT, AND
-    mask7 = _mm_and_si128(mask7, _mm_slli_epi64(maskO3, 9)); // SHIFT, AND
-    mask8 = _mm_and_si128(mask8, _mm_srli_epi64(maskO3, 9)); // SHIFT, AND
-
-	flip1 = _mm_or_si128(flip1, _mm_and_si128(mask1, _mm_slli_epi64(flip1, 2)));  // SHIFT, AND, OR
-	flip2 = _mm_or_si128(flip2, _mm_and_si128(mask2, _mm_srli_epi64(flip2, 2)));  // SHIFT, AND, OR
-	flip3 = _mm_or_si128(flip3, _mm_and_si128(mask3, _mm_slli_epi64(flip3, 16))); // SHIFT, AND, OR
-	flip4 = _mm_or_si128(flip4, _mm_and_si128(mask4, _mm_srli_epi64(flip4, 16))); // SHIFT, AND, OR
-	flip5 = _mm_or_si128(flip5, _mm_and_si128(mask5, _mm_slli_epi64(flip5, 14))); // SHIFT, AND, OR
-	flip6 = _mm_or_si128(flip6, _mm_and_si128(mask6, _mm_srli_epi64(flip6, 14))); // SHIFT, AND, OR
-	flip7 = _mm_or_si128(flip7, _mm_and_si128(mask7, _mm_slli_epi64(flip7, 18))); // SHIFT, AND, OR
-	flip8 = _mm_or_si128(flip8, _mm_and_si128(mask8, _mm_srli_epi64(flip8, 18))); // SHIFT, AND, OR
-
-    mask1 = _mm_and_si128(mask1, _mm_slli_epi64(mask1, 2));	 // SHIFT, AND
-    mask2 = _mm_and_si128(mask2, _mm_srli_epi64(mask2, 2));	 // SHIFT, AND
-    mask3 = _mm_and_si128(mask3, _mm_slli_epi64(mask3, 16)); // SHIFT, AND
-    mask4 = _mm_and_si128(mask4, _mm_srli_epi64(mask4, 16)); // SHIFT, AND
-    mask5 = _mm_and_si128(mask5, _mm_slli_epi64(mask5, 14)); // SHIFT, AND
-    mask6 = _mm_and_si128(mask6, _mm_srli_epi64(mask6, 14)); // SHIFT, AND
-    mask7 = _mm_and_si128(mask7, _mm_slli_epi64(mask7, 18)); // SHIFT, AND
-    mask8 = _mm_and_si128(mask8, _mm_srli_epi64(mask8, 18)); // SHIFT, AND
-
-	flip1 = _mm_or_si128(flip1, _mm_and_si128(mask1, _mm_slli_epi64(flip1, 4)));  // SHIFT, AND, OR
-	flip2 = _mm_or_si128(flip2, _mm_and_si128(mask2, _mm_srli_epi64(flip2, 4)));  // SHIFT, AND, OR
-	flip3 = _mm_or_si128(flip3, _mm_and_si128(mask3, _mm_slli_epi64(flip3, 32))); // SHIFT, AND, OR
-	flip4 = _mm_or_si128(flip4, _mm_and_si128(mask4, _mm_srli_epi64(flip4, 32))); // SHIFT, AND, OR
-	flip5 = _mm_or_si128(flip5, _mm_and_si128(mask5, _mm_slli_epi64(flip5, 28))); // SHIFT, AND, OR
-	flip6 = _mm_or_si128(flip6, _mm_and_si128(mask6, _mm_srli_epi64(flip6, 28))); // SHIFT, AND, OR
-	flip7 = _mm_or_si128(flip7, _mm_and_si128(mask7, _mm_slli_epi64(flip7, 36))); // SHIFT, AND, OR
-	flip8 = _mm_or_si128(flip8, _mm_and_si128(mask8, _mm_srli_epi64(flip8, 36))); // SHIFT, AND, OR
-
-	//ANDNOT, 8xOR, 8xSHIFT, 8xAND
-	mask1 = _mm_andnot_si128(
-					_mm_or_si128(A, B), //~BitBoardEmpty
-					_mm_or_si128(
-						_mm_or_si128(
-							_mm_or_si128(
-								_mm_slli_epi64(_mm_and_si128(flip1, maskO1), 1),
-								_mm_srli_epi64(_mm_and_si128(flip2, maskO1), 1)
-							),
-							_mm_or_si128(
-								_mm_slli_epi64(_mm_and_si128(flip3, maskO2), 8),
-								_mm_srli_epi64(_mm_and_si128(flip4, maskO2), 8)
-							)
-						),
-						_mm_or_si128(
-							_mm_or_si128(
-								_mm_slli_epi64(_mm_and_si128(flip5, maskO3), 7),
-								_mm_srli_epi64(_mm_and_si128(flip6, maskO3), 7)
-							),
-							_mm_or_si128(
-								_mm_slli_epi64(_mm_and_si128(flip7, maskO3), 9),
-								_mm_srli_epi64(_mm_and_si128(flip8, maskO3), 9)
-							)
-						)
-					)
-				);
-	PossibleMovesP = ((unsigned long long *)&mask1)[1];
-	PossibleMovesO = ((unsigned long long *)&mask1)[0];
-}
-
-void PlayStone(unsigned long long & P, unsigned long long & O, const unsigned char coordinate)
-{
-	unsigned long long flipped = flip(P, O, coordinate);
-	P ^= flipped ^ (1ULL << coordinate);
-	O ^= flipped;
-	std::swap(P, O);
-}
-
-unsigned long long StableStones_full_edges_second_order(const unsigned long long P, const unsigned long long O)
-{
-    unsigned long long StableStones, tmpBitBoard;
-    unsigned long long BitBoardOccupied = P | O;
+    uint64_t StableStones, tmpBitBoard;
+    uint64_t BitBoardOccupied = P | O;
     StableStones = 0;
 
     if ((BitBoardOccupied & 0x00000000000000FFULL) == 0x00000000000000FFULL)
@@ -142,9 +46,9 @@ unsigned long long StableStones_full_edges_second_order(const unsigned long long
     return StableStones;
 }
 
-unsigned long long StableStones_triangles(const unsigned long long O)
+uint64_t StableStones_triangles(const uint64_t O)
 {
-    unsigned long long StableStones;
+    uint64_t StableStones;
 
     if ((O & 0x0000000000000001ULL) == 0x0000000000000001ULL) // Triangle of Size 1
     {
@@ -303,10 +207,10 @@ unsigned long long StableStones_triangles(const unsigned long long O)
     return StableStones;
 }
 
-unsigned long long StableStones_skyline(unsigned long long O)
+uint64_t StableStones_skyline(uint64_t O)
 {
     int StablesOld, StablesNew;
-    unsigned long long tmpBitBoard, StableStones;
+    uint64_t tmpBitBoard, StableStones;
 	StableStones = 0x0000000000000000ULL;
 		
 	for (int directions = 0; directions < 8; ++directions)
