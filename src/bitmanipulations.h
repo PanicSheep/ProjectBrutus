afbcb6f8b78f78e6ac42196510137f4a230fcf2b
PanicSheep <PanicSheep@users.noreply.github.com>
2015-05-06 15:20:29 +0200

    all clean and tidy

--- src/bitmanipulations.h
+++ src/bitmanipulations.h
@@ -1,12 +1,28 @@
 #pragma once
-#include <cassert>
-#include <intrin.h>
+#include <cstdint>
+
+#if defined(_MSC_VER)
+	#include <intrin.h>
+#elif defined(__GNUC__)
+	#include <x86intrin.h>
+#else
+	#error compiler not supported
+#endif
+
+// Predefined macros hell:
+//
+// __GNUC__           Compiler is gcc, Clang or Intel on Linux
+// __INTEL_COMPILER   Compiler is Intel
+// _MSC_VER           Compiler is MSVC or Intel on Windows
+// _WIN32             Building on Windows (any)
+// _WIN64             Building on Windows 64 bit
 
 // #####################################
 // ##    CPU specific optimizations   ##
 // #####################################
 //#define YORKFIELD
-#define HASWELL
+//#define HASWELL_ULT
+//#define HASWELL_DT
 
 //#define HAS_MMX
 //#define HAS_SSE
@@ -23,7 +39,7 @@
 #define CACHE_LINE_SIZE 64
 // #####################################
 
-// CPU instruction set implications
+// CPUs instruction sets
 #ifdef YORKFIELD
 	#define HAS_MMX
 	#define HAS_SSE
@@ -32,7 +48,18 @@
 	#define HAS_SSSE3
 	#define HAS_SSE4_1
 #endif
-#ifdef HASWELL
+#ifdef HASWELL_ULT
+	#define HAS_MMX
+	#define HAS_SSE
+	#define HAS_SSE2
+	#define HAS_SSE3
+	#define HAS_SSSE3
+	#define HAS_SSE4_1
+	#define HAS_SSE4_2
+	#define HAS_AVX
+	#define HAS_AVX2
+#endif
+#ifdef HASWELL_DT
 	#define HAS_MMX
 	#define HAS_SSE
 	#define HAS_SSE2
@@ -55,6 +82,7 @@
 	#define HAS_BLSI // Extract Lowest Set Isolated Bit (x & -x)
 	#define HAS_BLSMASK // Get mask up to lowest set bit (x ^ (x - 1))
 	#define HAS_BLSR // Reset lowest set bit (x & (x - 1))
+	#define HAS_LZCNT
 #endif
 #if defined(HAS_BMI2)
 	#define HAS_PDEP // Parallel bits deposit
@@ -62,10 +90,8 @@
 #endif
 
 // alignas work-around
-#if defined(_MSC_VER)
-#define alignas(A) __declspec(align(A))
-#elif (__GNUC__==4) && (__GNUC_MINOR__ <= 7) 
-#define alignas(A) __attribute__((aligned(A)))
+#if (__GNUC__ == 4) && (__GNUC_MINOR__ <= 7)
+	#define alignas(A) __attribute__((aligned(A)))
 #endif
 
 // CACHE_LINE_ALIGNMENT
@@ -87,86 +113,104 @@
 
 
 // BitScan
+// OUT: mask == 0 results in undefined index
 #if defined(_MSC_VER)
 	#pragma intrinsic(_BitScanForward64)
+	#pragma intrinsic(_BitScanForward)
 	#pragma intrinsic(_BitScanReverse64)
+	#pragma intrinsic(_BitScanReverse)
 
-	FORCE_INLINE unsigned char BitScanLSB(unsigned long * index, const unsigned long long mask) { return _BitScanForward64(index, mask); }
-	FORCE_INLINE unsigned long BitScanLSB(const unsigned long long mask){
-		unsigned long index;
-		assert(mask != 0);
-		_BitScanForward64(&index, mask);
-		return index;
-	}
-	FORCE_INLINE unsigned char BitScanMSB(unsigned long * index, const unsigned long long mask) { return _BitScanReverse64(index, mask); }
-	FORCE_INLINE unsigned long BitScanMSB(const unsigned long long mask){
-		unsigned long index;
-		assert(mask != 0);
-		_BitScanReverse64(&index, mask);
-		return index;
-	}
+	FORCE_INLINE unsigned char BitScanLSB(unsigned long * index, const uint64_t mask) { return _BitScanForward64(index, mask); }
+	FORCE_INLINE unsigned char BitScanLSB(unsigned long * index, const uint32_t mask) { return _BitScanForward  (index, mask); }
+	FORCE_INLINE unsigned long BitScanLSB(const uint64_t mask){	unsigned long index; _BitScanForward64(&index, mask); return index; }
+	FORCE_INLINE unsigned long BitScanLSB(const uint32_t mask){	unsigned long index; _BitScanForward  (&index, mask); return index;	}
+	template <typename T> FORCE_INLINE unsigned char bsf(unsigned long * index, const T mask) { return BitScanLSB(index, mask); }
+
+	FORCE_INLINE unsigned char BitScanMSB(unsigned long * index, const uint64_t mask) { return _BitScanReverse64(index, mask); }
+	FORCE_INLINE unsigned char BitScanMSB(unsigned long * index, const uint32_t mask) { return _BitScanReverse  (index, mask); }
+	FORCE_INLINE unsigned long BitScanMSB(const uint64_t mask){ unsigned long index; _BitScanReverse64(&index, mask); return index; }
+	FORCE_INLINE unsigned long BitScanMSB(const uint32_t mask){	unsigned long index; _BitScanReverse  (&index, mask); return index; }
+	template <typename T> FORCE_INLINE unsigned char bsr(unsigned long * index, const T mask) { return BitScanMSB(index, mask); }
 #elif defined(__GNUC__)
-	inline unsigned long BitScanLSB(unsigned long long & b){
-		unsigned long long Ret;
-		assert(b != 0);
-		__asm__
-		(
-			"bsfq %[Input], %[Ret]"
-			:[Ret] "=r" (Ret)
-			:[b] "mr" (b)
-		);
-		return static_cast<unsigned long>(Ret);
-	}
-	inline unsigned long BitScanMSB(unsigned long long & b){
-		unsigned long long Ret;
-		assert(b != 0);
-		__asm__
-		(
-			"bsrq %[Input], %[Ret]"
-			:[Ret] "=r" (Ret)
-			:[b] "mr" (b)
-		);
-		return static_cast<unsigned long>(Ret);
-	}
+	FORCE_INLINE unsigned char BitScanLSB(unsigned long * index, const uint64_t mask) { *index = __builtin_ctzll(mask); return mask; }
+	FORCE_INLINE unsigned char BitScanLSB(unsigned long * index, const uint32_t mask) { *index = __builtin_ctz  (mask); return mask; }
+	FORCE_INLINE unsigned long BitScanLSB(const uint64_t mask){ return __builtin_ctzll(mask); }
+	FORCE_INLINE unsigned long BitScanLSB(const uint32_t mask){ return __builtin_ctz  (mask); }
+	template <typename T> FORCE_INLINE unsigned char bsf(unsigned long * index, const T mask) { return BitScanLSB(index, mask); }
+
+	FORCE_INLINE unsigned char BitScanMSB(unsigned long * index, const uint64_t mask) { *index = __builtin_clzll(mask) ^ 63; return mask; }
+	FORCE_INLINE unsigned char BitScanMSB(unsigned long * index, const uint32_t mask) { *index = __builtin_clz  (mask) ^ 31; return mask; }
+	FORCE_INLINE unsigned long BitScanMSB(const uint64_t mask){ return __builtin_clzll(mask) ^ 63; }
+	FORCE_INLINE unsigned long BitScanMSB(const uint32_t mask){ return __builtin_clz  (mask) ^ 31; }
+	template <typename T> FORCE_INLINE unsigned char bsr(unsigned long * index, const T mask) { return BitScanMSB(index, mask); }
 #endif
+
+
+// Count Leading Zeros
+#if defined(_MSC_VER)
+	#ifdef HAS_LZCNT
+		FORCE_INLINE uint64_t CountLeadingZeros(const uint64_t mask) { return __lzcnt64(mask); }
+		FORCE_INLINE uint32_t CountLeadingZeros(const uint32_t mask) { return __lzcnt  (mask); }
+	#else
+		FORCE_INLINE uint64_t CountLeadingZeros(const uint64_t mask) { return 63 - BitScanMSB(mask); }
+		FORCE_INLINE uint32_t CountLeadingZeros(const uint32_t mask) { return 31 - BitScanMSB(mask); }
+	#endif
+#elif defined(__GNUC__)
+		FORCE_INLINE uint64_t CountLeadingZeros(const uint64_t mask) { return __builtin_clzll(mask); }
+		FORCE_INLINE uint32_t CountLeadingZeros(const uint32_t mask) { return __builtin_clz  (mask); }
+#endif
+		template <typename T> FORCE_INLINE T clz(const T mask) { return CountLeadingZeros(mask); }
+
+// Count Trailing Zeros
+// OUT: result for mask == 0 is undefined
+#if defined(_MSC_VER)
+	FORCE_INLINE uint64_t CountTrailingZeros(const uint64_t mask) { return BitScanLSB(mask); }
+	FORCE_INLINE uint32_t CountTrailingZeros(const uint32_t mask) { return BitScanLSB(mask); }
+#elif defined(__GNUC__)
+	FORCE_INLINE uint64_t CountTrailingZeros(const uint64_t mask) { return __builtin_ctzll(mask); }
+	FORCE_INLINE uint32_t CountTrailingZeros(const uint32_t mask) { return __builtin_ctz  (mask); }
+#endif
+	template <typename T> FORCE_INLINE T ctz(const T mask) { return CountTrailingZeros(mask); }
+
 
 #ifdef HAS_BLSI
-	FORCE_INLINE unsigned long long GetLSB(const unsigned long long b){ return _blsi_u64(b); }
+	FORCE_INLINE uint64_t GetLSB(const uint64_t b) { return _blsi_u64(b); }
 #else
-	FORCE_INLINE unsigned long long GetLSB(const unsigned long long b){ return b & -b; }
+	FORCE_INLINE uint64_t GetLSB(const uint64_t b) { return b & -b; }
 #endif
 
-FORCE_INLINE unsigned long long GetMSB(const unsigned long long b){ return 1ULL << BitScanMSB(b); }
+FORCE_INLINE uint64_t GetMSB(const uint64_t b) { return 1ULL << BitScanMSB(b); }
 
 #ifdef HAS_BLSR
-	FORCE_INLINE void RemoveLSB(unsigned long long & b){ b = _blsr_u64(b); }
+	FORCE_INLINE void RemoveLSB(uint64_t & b) { b = _blsr_u64(b); }
 #else
-	FORCE_INLINE void RemoveLSB(unsigned long long & b){ b &= (b - 1); }
+	FORCE_INLINE void RemoveLSB(uint64_t & b) { b &= b - 1; }
 #endif
 
-FORCE_INLINE void RemoveMSB(unsigned long long & b){ b ^= GetMSB(b); }
+FORCE_INLINE void RemoveMSB(uint64_t & b) { b ^= GetMSB(b); }
 
 
 // PopCount
 #ifdef HAS_POPCNT
 	#if defined(_MSC_VER) || defined(__INTEL_COMPILER)
-		FORCE_INLINE unsigned long long PopCount(const unsigned long long b) { return _mm_popcnt_u64(b); }
-		FORCE_INLINE unsigned long long PopCount_max15(const unsigned long long b) { return PopCount(b); }
+		FORCE_INLINE uint64_t PopCount(const uint64_t b) { return _mm_popcnt_u64(b); }
+		FORCE_INLINE uint32_t PopCount(const uint32_t b) { return _mm_popcnt_u32(b); }
+		FORCE_INLINE uint64_t PopCount_max15(const uint64_t b) { return PopCount(b); }
+		FORCE_INLINE uint32_t PopCount_max15(const uint32_t b) { return PopCount(b); }
 	#else
-		FORCE_INLINE unsigned long long PopCount(unsigned long long b){
-			__asm__("popcnt %1, %0" : "=r" (b) : "r" (b));
-			return b;
-		}
-		FORCE_INLINE unsigned long long PopCount_max15(unsigned long long b) { PopCount(b); }
+		FORCE_INLINE uint64_t PopCount(uint64_t b){ return __builtin_popcountll(b); }
+		FORCE_INLINE uint32_t PopCount(uint32_t b){ return __builtin_popcount  (b); }
+		FORCE_INLINE uint64_t PopCount_max15(uint64_t b) { PopCount(b); }
+		FORCE_INLINE uint32_t PopCount_max15(uint32_t b) { PopCount(b); }
 	#endif
 #else
-	inline unsigned long long PopCount(unsigned long long b){
+	inline uint64_t PopCount(uint64_t b){
 		b -=  (b >> 1) & 0x5555555555555555ULL;
 		b  = ((b >> 2) & 0x3333333333333333ULL) + (b & 0x3333333333333333ULL);
 		b  = ((b >> 4) + b) & 0x0F0F0F0F0F0F0F0FULL;
 		return (b * 0x0101010101010101ULL) >> 56;
 	}
-	inline unsigned long long PopCount_max15(unsigned long long b){
+	inline uint64_t PopCount_max15(uint64_t b){
 		b -=  (b >> 1) & 0x5555555555555555ULL;
 		b  = ((b >> 2) & 0x3333333333333333ULL) + (b & 0x3333333333333333ULL);
 		return (b * 0x1111111111111111ULL) >> 60;
@@ -174,17 +218,33 @@ FORCE_INLINE void RemoveMSB(unsigned long long & b){ b ^= GetMSB(b); }
 #endif
 
 
+// BExtr
+#ifdef HAS_BEXTR
+	FORCE_INLINE uint64_t BExtr(const uint64_t src, const unsigned int start, unsigned int len) { return _bextr_u64(src, start, len); }
+#else
+	FORCE_INLINE uint64_t BExtr(const uint64_t src, const unsigned int start, unsigned int len) { return (src >> start) & ((1ULL << len)-1); }
+#endif
+
+
+// BZHI
+#ifdef HAS_BZHI
+	FORCE_INLINE uint64_t BZHI(const uint64_t src, const uint64_t index) { return _bzhi_u64(src, index); }
+#else
+	FORCE_INLINE uint64_t BZHI(const uint64_t src, const uint64_t index) { return src & ((1ULL << index) - 1); }
+#endif
+
+
 // PDep
 #ifdef HAS_PDEP
-	FORCE_INLINE unsigned long long PDep(const unsigned long long src, const unsigned long long mask) { return _pdep_u64(src, mask); }
+	FORCE_INLINE uint64_t PDep(const uint64_t src, const uint64_t mask) { return _pdep_u64(src, mask); }
 #else
-	inline unsigned long long PDep(unsigned long long src, unsigned long long mask){
-		unsigned long long res = 0;
-		for (unsigned long long bb = 1; mask; bb += bb)
+	inline uint64_t PDep(uint64_t src, uint64_t mask){
+		uint64_t res = 0;
+		for (uint64_t bb = 1; mask; bb += bb)
 		{
 			if (src & bb)
-				res |= mask & -mask;
-			mask &= mask - 1;
+				res |= GetLSB(mask);
+			RemoveLSB(mask);
 		}
 		return res;
 	}
@@ -193,24 +253,23 @@ FORCE_INLINE void RemoveMSB(unsigned long long & b){ b ^= GetMSB(b); }
 
 // PExt
 #ifdef HAS_PEXT
-	FORCE_INLINE unsigned long long PExt(const unsigned long long src, const unsigned long long mask) { return _pext_u64(src, mask); }
+	FORCE_INLINE uint64_t PExt(const uint64_t src, const uint64_t mask) { return _pext_u64(src, mask); }
 #else
-	inline unsigned long long PExt(unsigned long long src, unsigned long long mask){
-		unsigned long long res = 0;
-		for (unsigned long long bb = 1; mask; bb += bb)
+	inline uint64_t PExt(uint64_t src, uint64_t mask){
+		uint64_t res = 0;
+		for (uint64_t bb = 1; mask; bb += bb)
 		{
-			if (src & mask & -mask)
+			if (src & GetLSB(mask))
 				res |= bb;
-			mask &= mask - 1;
+			RemoveLSB(mask);
 		}
 		return res;
 	}
 #endif
 
-
-// BExtr
-#ifdef HAS_BEXTR
-	FORCE_INLINE unsigned long long BExtr(const unsigned long long src, const unsigned int start, unsigned int len) { return _bextr_u64(src, start, len); }
-#else
-	FORCE_INLINE unsigned long long BExtr(const unsigned long long src, const unsigned int start, unsigned int len) { return (src >> start) & ((1ULL << len)-1); }
+// BSWAP
+#if defined(_MSC_VER)
+	FORCE_INLINE uint64_t BSwap(const uint64_t b) { return _byteswap_uint64(b); }
+#elif defined(__GNUC__)
+	FORCE_INLINE uint64_t BSwap(const uint64_t b) { return __builtin_bswap64(b); }
 #endif
